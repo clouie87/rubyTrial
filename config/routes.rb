7fe61ce572ce6df153906d4f1228f1814bbce3a4
clouie87 <carol.louie@gmail.com>
2015-05-29 14:36:50 -0300

    refactoring the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :videos
+
   get 'pages/about'
 
   get 'pages/contact'
---
+++ db/migrate/20150529122703_delete_add.rb
@@ -0,0 +1,4 @@
+class DeleteAdd < ActiveRecord::Migration
+  def change
+  end
+end
